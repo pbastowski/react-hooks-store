b998ac80ea02890e85063659ff7d07c70687a89c
Paul Bastowski <pbastowski@gmail.com>
2020-11-15 18:00:36 +0100

    use vite bundler

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import reactPlugin from 'vite-plugin-react'
  * @type { import('vite').UserConfig }
  */
 const config = {
-    jsx: 'react',
-    plugins: [reactPlugin]
+  jsx: 'react',
+  plugins: [reactPlugin]
 }
 
 export default config
